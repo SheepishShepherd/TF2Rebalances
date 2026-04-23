f5e80a737fdd46c3bfc356eca8941fe1da4f5894
Sheepish Shepherd <cameronscottmcdougal@gmail.com>
2026-04-23 00:00:26 -0400

    changes to navigational elements
    * Bookmark scrolling is smoother
    * Bookmark links now have sounds
    * Back to Top button is now created via javascript and has been improved and isolated
    * Bookmark links no longer added to site history (no longer need to back out of all the viewed bookmarks to get to main menu)
    * Main menu improved
    * Disabled main menu buttons that linked to nonexistent pages (and removed the economy ones for now)

---
+++ nav_setup.js
@@ -0,0 +1,56 @@
+// Adds the back to top button
+const topPanel = document.getElementById("back-to-top-panel");
+var topButton = document.createElement("div");
+topButton.classList.add("img-nav-btn", "nav-btn", "nav-top");
+topButton.title = "Back to top";
+topButton.addEventListener("click", function () {
+	history.replaceState(null, document.title, window.location.pathname + window.location.search);
+	window.scrollTo(0, 0);
+	var audio = new Audio('../resources/sounds/hint.wav');
+	audio.currentTime = 0;
+	audio.volume = 0.2;
+	audio.play();
+});
+topPanel.appendChild(topButton);
+
+// sets up bookmark redirects and audio cues for all navigational buttons
+const nav_buttons = document.querySelectorAll('.nav-panel .nav-item'); // selects all elements with a 'nav-item' class and a 'nav-panel' class for its parent element
+nav_buttons.forEach(button => {
+	var bookmark = button.id.replace("nav-", "");
+	var soundPath = "";
+	if (button.parentElement.id == "class-selection") {
+		if (button.parentElement.classList.contains("mvm-nav")) {
+			soundPath = "mvm_class_menu_" + bookmark.replace("_Upgrades", "");
+		}
+		else {
+			soundPath = "class_menu_" + bookmark;
+		}
+	}
+	else if (button.parentElement.id == "nav-bookmarks") {
+		if (button.parentElement.classList.contains("mvm-nav")) {
+			soundPath = "mvm_warning";
+		}
+		else if (button.parentElement.classList.contains("halloween-nav")) {
+			soundPath = "clock_tick";
+		}
+		else {
+			soundPath = "slide_up";
+		}
+	}
+
+	button.addEventListener('click', () => {
+		location.replace('#' + bookmark)
+		var audio = new Audio('../resources/sounds/' + soundPath + '.wav');
+		audio.currentTime = 0;
+		audio.volume = 0.2;
+		audio.play();
+	});
+});
+
+// MvM weapon icons link to their respective rebalanced stat cards on weapons.html
+const elements = document.querySelectorAll('.mvm-mini-item');
+elements.forEach(element => {
+	element.addEventListener('click', function () {
+		window.location = "weapons.html#" + element.title.replaceAll(" ", "_");
+	});
+});
